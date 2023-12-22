5b306aadc5943dcdf14d38d8b4ac5686300c420e
NinoCucina <lpham6038@gmail.com>
2023-12-22 09:32:44 +0700

    Dash

--- app/src/main/java/com/example/recipesapp/activities/Dashboard.java
+++ app/src/main/java/com/example/recipesapp/activities/Dashboard.java
@@ -1,23 +1,51 @@
-package com.example.recipesapp.activities;
+package com.example.appcongthucnauan2023.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
-import com.example.recipesapp.R;
+import com.example.appcongthucnauan2023.R;
 
 public class Dashboard extends AppCompatActivity {
 
+    @SuppressLint("MissingSuperCall")
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        finishAndRemoveTask();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            finishAndRemoveTask();
+        }
+
         this.finishAffinity();
+
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    public void ViewAll(View view) {
+
+        Intent intent = new Intent(Dashboard.this,MainActivity.class);
+
+        switch (view.getId())
+        {
+            case R.id.monMan:
+                intent.putExtra("condition",1);
+                break;
+            case R.id.monChay:
+                intent.putExtra("condition",2);
+                break;
+
+        }
+
+        startActivity(intent);
+
     }
 }
