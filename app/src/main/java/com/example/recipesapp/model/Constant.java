c75828297fcbf7ea3117b702d7575f3ed13853bc
aGymerDev <huya2k22@gmail.com>
2023-12-22 10:40:50 +0700

    Update Constant.java

--- app/src/main/java/com/example/recipesapp/model/Constant.java
+++ app/src/main/java/com/example/recipesapp/model/Constant.java
@@ -1,4 +1,33 @@
-package com.example.recipesapp.model;
+package com.example.appcongthucnauan2023.model;
 
-public class Constant {
+public class Constant
+{
+    int image;
+    String title;
+    String ingredient;
+    String implementationstep;
+
+
+    public Constant(int image, String title, String ingredient, String implementationstep) {
+        this.image = image;
+        this.title = title;
+        this.ingredient = ingredient;
+        this.implementationstep = implementationstep;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public String getImplementationstep() {
+        return implementationstep;
+    }
 }
